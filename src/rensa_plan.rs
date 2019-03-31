f96e139a66543b00377c09f7573da59217c691f0
user <ume.drink@gmail.com>
2019-04-01 01:55:52 +0900

    obstacle

--- src/rensa_plan.rs
+++ src/rensa_plan.rs
@@ -6,16 +6,17 @@ use super::rand;
 use std::collections::VecDeque;
 
 const DEAD_LINE_Y: i32 = 16;
+const W: i32 = 10;
 
 #[derive(Clone, Default)]
 struct BeamState {
     board: board::Board,
-    score: u32,
+    score: i32,
     actions: Vec<u8>,
 }
 
 impl BeamState {
-    fn new(board: board::Board, score: u32, actions: Vec<u8>) -> Self {
+    fn new(board: board::Board, score: i32, actions: Vec<u8>) -> Self {
         Self { board, score, actions }
     }
 }
@@ -38,33 +39,42 @@ impl RensaPlan {
     pub fn set_pack(&mut self, packs: Vec<[[u8; 2]; 2]>) {
         self.packs = packs;
     }
+
+    fn is_dangerous(board: &board::Board) -> bool {
+        board.max_height() as i32 >= DEAD_LINE_Y - 1
+    }
     
-    pub fn calc_rensa_plan(&mut self, cur_turn: usize, board: &board::Board) {
+    pub fn calc_rensa_plan(&mut self, cur_turn: usize, board: &board::Board, obstacle_stock: i32) {
         let mut next = Vec::new();
         let mut cur = vec![BeamState::new(board.clone(), 0, Vec::new())];
-        let fire_turn = 15;
-        let beam_width = 100;
+        let fire_turn = 20;
+        let beam_width = 100 * 3;
         let actions = action::Action::all_actions();
+        let allow_dead_line = Self::is_dangerous(board);
+        let fall = self.packs[cur_turn + fire_turn].clone();
         for i in 0..fire_turn {
             let turn = cur_turn + i;
             let pack = self.packs[turn].clone();
-            let fall = self.packs[cur_turn + fire_turn].clone();
+            let rest_obstacle = obstacle_stock - i as i32 * W;
             cur.iter().for_each(|b| {
                 actions.iter().for_each(|a| {
                     if let action::Action::PutBlock { pos, rot } = *a {
                         let mut board = b.board.clone();
+                        if rest_obstacle >= W {
+                            board.fall_obstacle();
+                        }
                         board.put(&pack, pos, rot);
                         if board.is_dead() {
                             return;
                         }
 
                         // obstacleが降ってくると危ないので
-                        if board.max_height() as i32 >= DEAD_LINE_Y - 1 {
+                        if !allow_dead_line && Self::is_dangerous(&board) {
                             return;
                         }
                         let mut rensa_eval_board = board.clone();
-                        let rensa = rensa_eval_board.put(&fall, 0, 0) as u32;
-                        let score = rensa * 10000 + (self.rand.next() & 0xF) as u32;
+                        let rensa = rensa_eval_board.put(&fall, 0, 0) as i32;
+                        let score = rensa * 10000 + (self.rand.next() & 0xF) as i32 - board.max_height() as i32;
                         let mut actions = b.actions.clone();
                         actions.push(a.into());
                         next.push(BeamState::new(board, score, actions));
@@ -77,7 +87,7 @@ impl RensaPlan {
             // eprintln!("search: {} {}", i, cur[0].score);
             next.clear();
         }
-        eprintln!("best: {}", cur[0].score);
+        eprintln!("best: {} {} {} {}", cur_turn, cur[0].score, cur[0].actions.len(), obstacle_stock);
         let best = &cur[0].actions;
         self.replay = best.iter().map(|s| action::Action::from(*s)).collect();
         self.replay.push_back(action::Action::PutBlock{ pos: 0, rot: 0, })
