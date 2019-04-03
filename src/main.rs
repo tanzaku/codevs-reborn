d4a77488b0428eef40515d57ab47f4ef59031f0d
user <ume.drink@gmail.com>
2019-04-04 00:53:08 +0900

    skill ai

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,10 @@
 
 mod action;
 mod board;
+mod skill_plan;
 mod rensa_plan;
 mod rensa_ai;
+mod skill_ai;
 mod rand;
 mod score_calculator;
 mod player;
@@ -15,5 +17,6 @@ fn main() {
     let stdin = std::io::stdin();
     let lock = stdin.lock();
     let mut ai = rensa_ai::RensaAi::new(lock);
+    // let mut ai = skill_ai::RensaAi::new(lock);
     ai.exec();
 }
