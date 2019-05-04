0c072141aed0ded6c05b0e2436df4bcd15870ece
user <ume.drink@gmail.com>
2019-05-04 23:20:25 +0900

    v0.10

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,11 @@
 
-#![allow(dead_code)]
-// #![allow(unused_variables)]
-
 extern crate codevs_reborn_lib;
 
-// mod lambda_client;
-
-// mod action;
-// mod board;
-// mod skill_plan;
-// mod rensa_plan;
-// mod rensa_ai;
-// mod skill_ai;
-// mod rand;
-// mod score_calculator;
-// mod player;
-
-use codevs_reborn_lib::rensa_ai;
 use codevs_reborn_lib::best_ai;
 
 fn main() {
-    // if true {
-    //     lambda_client::test();
-    //     return;
-    // }
-
     let stdin = std::io::stdin();
     let lock = stdin.lock();
-    // let mut ai = rensa_ai::RensaAi::new(lock);
-    // let mut ai = skill_ai::SkillAi::new(lock);
     let mut ai = best_ai::BestAi::new(lock);
     ai.exec();
 }
